e1c63c9756e4230e6738dd457925ec0e35f6294a
Brandon Smith <brandon.smith@yale.edu>
2013-08-25 16:18:09 -0400

    Initial Commit

---
+++ Socialyte/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Socialyte
+//
+//  Created by Brandon Smith on 8/25/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Socialyte/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Socialyte
+//
+//  Created by Brandon Smith on 8/25/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
